c98128f333541ab5518b0c3b40adc25d6d7f32d9
momsad <momsad882@gmail.com>
2024-07-27 19:05:36 +0600

    Initial commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return (
+    <p>dashboard Page (Protected)</p>
+  );
+}
+
+export default DashboardPage;
